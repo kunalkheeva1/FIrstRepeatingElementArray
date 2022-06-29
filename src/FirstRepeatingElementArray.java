d222358ad70325402fe17890d05d3c9733d293f9
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-29 01:07:59 -0700

    created a method to get the repeated element with the smallest index

--- src/FirstRepeatingElementArray.java
+++ src/FirstRepeatingElementArray.java
@@ -1,5 +1,24 @@
+import java.util.HashMap;
+
 public class FirstRepeatingElementArray {
-    
+    public static int firstRepeatingElementArray(int [] arr, int n){
+        int mi = Integer.MAX_VALUE;
+        boolean rep = false;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<n; i++){
+            if(map.containsKey(arr[i])){
+                rep = true;
+                mi = Math.min(mi, map.get(arr[i]));
+            }
+            else{
+                map.put(arr[i], i+1);
+            }
+        }
+        if(rep == false){
+            return -1;
+        }
+        else return mi;
+    }
     public static void main(String[] args) {
 
     }
